364ed08d62699db53e39c02cc63752b72b23d03f
Narith2022 <thochannarith@gmail.com>
2024-07-20 18:29:57 +0700

    build button function already

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
 function process(operation){
     switch (operation){
-        case 'exit':
+        case'start':
+            document.getElementById('stop').disabled = false;
+            document.getElementById('start').disabled = true;
+            break;
+        case 'stop':
+            document.getElementById('stop').disabled = true;
+            document.getElementById('start').disabled = false;
+            break;
+        default:
             window.close();
     }
 }
